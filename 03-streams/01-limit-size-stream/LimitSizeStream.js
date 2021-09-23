3d0ba88d61160b27406d737cf6946487599f840d
Евгений Гузий <zhenya.guziy@gmail.com>
2021-09-23 12:53:54 +0300

    add solution for 03-streams/01-limit-size-stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,21 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.accumulator = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    try {
+      this.accumulator += chunk.length;
+      if (this.accumulator > this.limit) {
+        throw new LimitExceededError();
+      } else {
+        callback(null, chunk);
+      }
+    } catch (ex) {
+      callback(ex);
+    }
   }
 }
 
--- 03-streams/01-limit-size-stream/index.js
+++ 03-streams/01-limit-size-stream/index.js
@@ -8,6 +8,11 @@ limitedStream.pipe(outStream);
 
 limitedStream.write('hello'); // 'hello' - это 5 байт, поэтому эта строчка целиком записана в файл
 
+// обработчик ошибки от лимит стрима
+limitedStream.on('error', (error) => {
+  console.log(error.code);
+});
+
 setTimeout(() => {
   limitedStream.write('world'); // ошибка LimitExceeded! в файле осталось только hello
 }, 10);
