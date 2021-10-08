b425949464a978e13a6e02a954db149c7f3cd8e5
Евгений Гузий <zhenya.guziy@gmail.com>
2021-10-08 23:19:01 +0300

    add solution for 03-streams/03-file-server-get

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,4 +1,5 @@
 const url = require('url');
+const fs = require('fs');
 const http = require('http');
 const path = require('path');
 
@@ -12,7 +13,26 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
-
+      const readableStream = fs.createReadStream(filepath);
+      if (/\/+/.test(pathname)) {
+        res.statusCode = 400;
+        res.end('Attachment files not supported');
+      }
+      readableStream.on('data', (chunk) => {
+        res.write(chunk);
+      });
+      readableStream.on('end', () => {
+        res.end();
+      });
+      readableStream.on('error', (err) => {
+        if (err.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('File not found');
+        } else {
+          res.statusCode = 500;
+          res.end('Internal server error');
+        }
+      });
       break;
 
     default:
