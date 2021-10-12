0df41b634f45901f7499e7d696fdd8059085d475
Евгений Гузий <zhenya.guziy@gmail.com>
2021-10-12 16:16:13 +0300

    add solution for 03-streams/04-file-server-post

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -12,7 +14,51 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      if (/\/+/.test(pathname)) {
+        res.statusCode = 400;
+        res.end('Attachment files not supported');
+        return;
+      }
+      const writableStream = fs.createWriteStream(filepath, {
+        flags: 'wx',
+      });
+      const limitSizeStream = new LimitSizeStream({limit: 1000000});
+      req
+          .pipe(limitSizeStream)
+          .pipe(writableStream);
 
+      writableStream.on('error', (err) => {
+        if (err.code === 'EEXIST') {
+          res.statusCode = 409;
+          res.end('file already exist');
+          return;
+        }
+        res.statusCode = 500;
+        res.end('Internal server error');
+      });
+      writableStream.on('finish', () => {
+        res.statusCode = 201;
+        res.end('file saved');
+      });
+
+      limitSizeStream.on('error', (err) => {
+        console.log('error');
+        if (err.code === 'LIMIT_EXCEEDED') {
+          res.statusCode = 413;
+          res.end('file exceed size');
+        } else {
+          res.statusCode = 500;
+          res.end('Internal server error');
+        }
+        writableStream.destroy();
+        fs.unlink(filepath, (err) => {});
+      });
+
+      req.on('aborted', () => {
+        limitSizeStream.destroy();
+        writableStream.destroy();
+        fs.unlink(filepath, (err) => {});
+      });
       break;
 
     default:
